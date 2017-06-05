d928f9d3e13fe2a9a76ad8e157cdfd9ccf297123
amjltc295 <amjltc295@github.com>
2017-06-05 11:52:41 +0800

    Rewrite IO

--- dcard_getData.py
+++ dcard_getData.py
@@ -8,6 +8,9 @@ NTUEE3 B03
 
 from dcard import Dcard
 import pickle
+import re
+from dataIO import TypeResultWrapper, WordResultWrapper
+
 def help_message():
     print ("Psychoinfomatics and Neuroinformatics")
     print ("Final Project")
@@ -43,26 +46,65 @@ def get_words(metas):
     print ("")
 
     return filteredMetas
+"""
+class TypeResultWrapper:
+
+    def __init__(self, type_):
+        self.typeName = type_
+        self.wordList = []
+
+    def addWord(self, wordResultWrapper):
+        self.wordList.append(wordResultWrapper)
+
+    def printTypeResult(self):
+        print ("")
+        print ("#%s" % self.typeName)
+        for eachWord in self.wordList:
+            eachWord.printWordResult()
+
+class WordResultWrapper:
+
+    def __init__(self, word):
+        self.wordName = word
+        self.titleNum = 0
+        self.contentNum = 0
+        self.commentNum = 0
 
 
+    def printWordResult(self):
+        #Remove white space for Chinese characters
+        wordLen = len(self.wordName)
+        #English
+        if len(self.wordName) == len(self.wordName.encode()):
+            wordLen = 0
+        print ("%-*s %8d %8d %8d" % ((8-wordLen), self.wordName, self.titleNum, self.contentNum, self.commentNum))
 
+"""
 class DcardWrapper:
 
     def __init__(self):
         self.dcard = Dcard()
         self.word_list_dict = dict() #a dict with {"品德": ["母豬", "妓女"...], ...}
         self.forum_list = None
-        self.searching_num = 500
+        self.searching_num = 5
+        self.result_list = [] #a list of result dict
+
+    def printResult(self):
+        print ("")
+        print ("%-8s %8s %8s %8s" % ("Word in", "Title", "Content", "Comment"))
+        for eachType in self.result_list:
+            eachType.printTypeResult()
 
     def readTextList(self, text_list_file):
         text_list = text_list_file.read().split('\n')
 
+        print ("Text from file: ")
         print (text_list)
         listLen = len(text_list)
         i = 0
         while i  < listLen:
             if "#" in text_list[i]:
-                type_name = text_list[i].split("#")[0]
+                type_name = text_list[i].split("#")[1]
                 i += 1
                 a_word_type = []
                 while (text_list[i] != ''):
@@ -74,6 +116,7 @@ class DcardWrapper:
             else:
                 raise IOError("Error in read file, line %d: %s" % (i, text_list[i]))
 
+        print ("Word list dictionary read: ")
         print (self.word_list_dict)
 
     def readForumList(self, forum_list_file):
@@ -90,7 +133,7 @@ class DcardWrapper:
 
 
 
-    def getWordDataFromForum(self, forumName, word):
+    def getWordDataFromForum(self, forumName):
         #metadata_forums = dcard.forums.get()
         """
         for each in metadata_forums:
@@ -101,38 +144,49 @@ class DcardWrapper:
         word = '婊'
         """
         f = self.dcard.forums(forumName)
-        #print (f.get_metas())
-        print ("Searching ", word, " in ", forumName,  " ...")
+        print (self.searching_num, "Meta collecting ...", end=' ')
         m = f.get_metas(num=self.searching_num)#, callback=get_words) #list
-        print (self.searching_num, "Meta collecting done.")
+        print ("Done.")
+        print ("Posts collecting ...", end=' ')
         p = self.dcard.posts(m).get(links=False)
-        print ("Posts collecting done.")
-        titleNum = 0
-        commentNum = 0
-        count = 0
+        print ("Done.")
+        print ("Result parsing ...", end=' ')
         result = p.result()
-        print ("Result parsing done.")
-        for eachPost in result:
-            print ("Searching %d / %d ..." % (count, len(result)), end='\r')
-            count += 1
-            #print (eachPost['comments'])
-            if (word in eachPost['title']):
-                titleNum+= 1
-                print (eachPost['title'])
-            for eachComment in eachPost['comments']:
-                if not eachComment['hidden']:
-                    if (word in eachComment['content']):
-                        commentNum += 1
-                        print (eachComment)
+        print ("Done.")
+        for typeName, wordList in self.word_list_dict.items():
+            typeResultWrapper = TypeResultWrapper(typeName)
+            print ("")
+            print ("#Type: ", typeName)
+            for eachWord in wordList:
+                wordResultWrapper = WordResultWrapper(eachWord)
 
-        print ("Searching %d / %d ..." % (len(result), len(result)))
-        print ("Title:", titleNum)
-        print ("Comment:", commentNum)
+                count = 0
+                for eachPost in result:
+                    print ("Searching %s in %s ...  %d / %d " % (eachWord, forumName, count, len(result)), end='\r')
+                    count += 1
+                    #print (eachPost['comments'])
+                    wordInTitles = re.findall(eachWord, eachPost['title'])
+                    if wordInTitles:
+                        wordResultWrapper.titleNum += len(wordInTitles)
+                    wordInContent = re.findall(eachWord, eachPost['content'])
+                    if wordInContent:
+                        wordResultWrapper.contentNum += len(wordInContent)
+                    for eachComment in eachPost['comments']:
+                        if eachComment['hidden']:
+                            wordInComment = re.findall(eachWord, eachComment['content'])
+                            if not wordInComment:
+                                wordResultWrapper.commentNum += len(wordInComment)
+
+                print ("Searching %s in %s ...  %d / %d " % (eachWord, forumName, count, len(result)))
+                typeResultWrapper.addWord(wordResultWrapper)
+            self.result_list.append(typeResultWrapper)
+        self.printResult()
 
 def main():
     dcardWrapper = DcardWrapper()
     with open('word_list_female.txt', 'r') as text_list_file:
         dcardWrapper.readTextList(text_list_file)
+    dcardWrapper.getWordDataFromForum('sex')
 
     #data_to_write_file =
 
